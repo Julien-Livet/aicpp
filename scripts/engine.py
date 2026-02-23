1185fa1eae03115e0cc0ba7117eff0b92a09f530
Julien LIVET <julien.livet@univ-nantes.fr>
2026-02-23 11:14:44 +0100

    Fix script.py

--- scripts/engine.py
+++ scripts/engine.py
@@ -168,7 +168,7 @@ std::map<int, int> bar(int arg0, double arg1);"""
         f.write(command)
         f.close()
 
-        result = OpenAI().responses.create(model = "gpt-5-mini", input = command).output_text
+        result = OpenAI().responses.create(model = "gpt-5", input = command).output_text
         content = result
         f = open("output" + task + ".txt", "w")
         f.write(content)
