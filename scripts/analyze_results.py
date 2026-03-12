51be308a943bfd5817d1f75d10dabe289c794e67
Julien LIVET <julien.livet@univ-nantes.fr>
2026-03-12 11:04:34 +0100

    Analyze results with plots

---
+++ scripts/analyze_results.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def analyseFolder(folder: str):
+    assert(folder == "training" or folder == "evaluation")
+
+    f = open(folder + "_results.md")
+    content = f.read()
+    f.close()
+    
+    lines = list(filter(None, content.split("\n")))
+    
+    i = 0
+    labels = []
+    y1 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    yt = []
+
+    while (i < len(lines)):
+        f, t, s1, s2, s3, s4, st = lines[i].split(" ")
+        labels.append(t)
+        y1.append(float(s1))
+        y2.append(float(s2))
+        y3.append(float(s3))
+        y4.append(float(s4))
+        yt.append(float(st))
+
+        while (lines[i] != "```"):
+            i += 1
+
+        i += 1
+
+    x = range(0, len(labels))
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y1, label = "size_cost")
+    ax.plot(x, y2, label = "value_cost")
+    ax.plot(x, y3, label = "pixel_overlap_cost")
+    ax.plot(x, y4, label = "bounding_box_cost")
+    ax.plot(x, yt, label = "total_cost")
+    plt.xticks(x, labels, rotation = 'vertical')
+    plt.legend()
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
+
+analyseFolder("training")
+analyseFolder("evaluation")
+
