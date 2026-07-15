1027eb36431c1d31fc4ddf94c3a6c2b9af4573e9
Julien LIVET <julien.livet@free.fr>
2026-07-15 21:02:30 +0200

    Add templated Neuron class and enhance type handling with JSON serialization

--- include/aicpp/Neuron.h
+++ include/aicpp/Neuron.h
@@ -11,6 +11,98 @@
 
 namespace aicpp
 {
+    namespace typed
+    {
+        void addType(boost::json::array& inputs)
+        {
+        }
+
+        template <typename T, typename... Args>
+        void addType(boost::json::array& inputs)
+        {
+            inputs.emplace_back(std::string(typeid(T).name()));
+            addType<Args...>(inputs);
+        }
+ 
+        std::string typeDot(size_t startIndex, size_t& index, size_t count)
+        {
+            return std::string{};
+        }
+
+        template <typename T, typename... Args> std::string typeDot(size_t startIndex, size_t& index, size_t count)
+        {
+            std::string s{"n" + std::to_string(index) + " [label=\"" + typeid(T).name() + "\", shape=circle, style=fill];\n"};
+            s += "n" + std::to_string(index) + " -> n" + std::to_string(startIndex + count) + ";\n";
+            ++index;
+
+            s += typeDot<Args...>(startIndex, index, count);
+
+            return s;
+        }
+        template <typename T, typename... Args>
+        class Neuron
+        {
+            public:
+                Neuron(std::string const& name, std::function<T(Args...)> const& function) : name_{name}, function_{function}
+                {
+                }
+
+                std::string const& name() const
+                {
+                    return name_;
+                }
+
+                std::function<T(Args...)>& function()
+                {
+                    return function_;
+                }
+
+                std::function<T(Args...)> const& function() const
+                {
+                    return function_;
+                }
+                
+                std::pair<std::string, size_t> dot(size_t index = 0) const
+                {
+                    std::string s;
+                    size_t const startIndex{index};
+
+                    s += typeDot<Args...>(startIndex, index, std::tuple_size<std::tuple<Args...> >::value);
+                    s += "n" + std::to_string(index) + " [label=\"" + name_ + "\", shape=circle, style=fill];\n";
+                    ++index;
+
+                    s += "n" + std::to_string(index) + " [label=\"" + typeid(T).name() + "\", shape=circle, style=fill];\n";
+                    s += "n" + std::to_string(startIndex + std::tuple_size<std::tuple<Args...> >::value) + " -> n" + std::to_string(index) + ";\n";
+                    ++index;
+
+                    return std::make_pair(s, index);
+                }
+
+                boost::json::value toJson() const
+                {
+                    using namespace boost::json;
+
+                    object obj;
+
+                    obj["name"] = name_;
+
+                    array inputs;
+
+                    addType<Args...>(inputs);
+
+                    obj["inputTypes"] = std::move(inputs);
+
+                    obj["outputType"] = std::string(typeid(T).name());
+
+                    return obj;
+                }
+
+            private:
+                std::string name_;
+                std::function<T(Args...)> function_;
+        }
+    }
+
     class Neuron
     {
         public:
