ccf0fe26fe4447f60e84c6d6b65fbacc9d37cb3d
Julien LIVET <julien.livet@univ-nantes.fr>
2026-07-16 09:23:38 +0200

    Refactor buildConnection function to accept combined neuronsByOutputType and improve connection handling

--- src/dsl_dataset.cpp
+++ src/dsl_dataset.cpp
@@ -1,9 +1,10 @@
 #include <cassert>
 #include <iostream>
 #include <mutex>
+#include <print>
 #include <random>
+#include <set>
 #include <thread>
-#include <unordered_set>
 
 #include "aicpp/Connection.h"
 #include "aicpp/Hodel.h"
@@ -14,6 +15,7 @@ using namespace aicpp;
 
 Connection buildConnection(std::map<std::type_index, std::vector<Neuron> > const& variableNeuronsByOutputType,
                            std::map<std::type_index, std::vector<Neuron> > const& primitiveNeuronsByOutputType,
+                           std::map<std::type_index, std::vector<Neuron> > const& neuronsByOutputType,
                            size_t depth, std::type_index const& type)
 {
     std::random_device rd;
@@ -28,9 +30,6 @@ Connection buildConnection(std::map<std::type_index, std::vector<Neuron> > const
         return Connection{neuron, {}};
     }
 
-    std::map<std::type_index, std::vector<Neuron> > neuronsByOutputType{variableNeuronsByOutputType};
-    neuronsByOutputType.insert(primitiveNeuronsByOutputType.begin(), primitiveNeuronsByOutputType.end());
-
     std::uniform_int_distribution<size_t> dist(0, neuronsByOutputType.at(type).size() - 1);
     Neuron const& neuron{neuronsByOutputType.at(type).at(dist(rd))};
 
@@ -38,12 +37,20 @@ Connection buildConnection(std::map<std::type_index, std::vector<Neuron> > const
 
     for (auto const& inputType : neuron.inputTypes())
     {
-        Connection const& inputConnection{buildConnection(variableNeuronsByOutputType, primitiveNeuronsByOutputType, depth - 1, inputType)};
+        Connection const& inputConnection{buildConnection(variableNeuronsByOutputType, primitiveNeuronsByOutputType, neuronsByOutputType, depth - 1, inputType)};
         inputs.emplace_back(inputConnection);
     }
 
     return Connection{neuron, inputs};
 }
+
+struct ConnectionLess
+{
+    bool operator()(Connection const& x, Connection const& y) const
+    {
+        return x.hash() < y.hash();
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -65,26 +72,33 @@ int main(int argc, char* argv[])
     for (auto const& variable : variables)
         variableNeuronsByOutputType[variable.second.outputType()].emplace_back(variable.second);
 
+    Neuron iNeuron{"I", [] (std::vector<std::any> const&) -> std::any { return std::any{}; }, std::vector<std::type_index>{}, typeid(hodel::Grid)};
+
+    variableNeuronsByOutputType[iNeuron.outputType()].emplace_back(iNeuron);
+
     std::map<std::type_index, std::vector<Neuron> > primitiveNeuronsByOutputType;
 
     for (auto const& primitive : primitives)
         primitiveNeuronsByOutputType[primitive.second.outputType()].emplace_back(primitive.second);
 
-    std::unordered_set<Connection> connections;
+    std::map<std::type_index, std::vector<Neuron> > neuronsByOutputType{variableNeuronsByOutputType};
+    neuronsByOutputType.insert(primitiveNeuronsByOutputType.begin(), primitiveNeuronsByOutputType.end());
+
+    std::set<Connection, ConnectionLess> connections;
     std::mutex mutex;
 
-    auto const addConnection = [&mutex, &connections, variableNeuronsByOutputType, primitiveNeuronsByOutputType, depth, count] () {
+    auto const addConnection = [&mutex, &connections, &variableNeuronsByOutputType, &primitiveNeuronsByOutputType, &neuronsByOutputType, depth, count] () {
         if (connections.size() >= count)
             return;
 
         try
         {
-            auto const connection{buildConnection(variableNeuronsByOutputType, primitiveNeuronsByOutputType, depth, typeid(hodel::Grid))};
+            auto const connection{buildConnection(variableNeuronsByOutputType, primitiveNeuronsByOutputType, neuronsByOutputType, depth, typeid(hodel::Grid))};
 
             //...Check connection result
 
             std::lock_guard<std::mutex> lock(mutex);
-            connections.emplace(connection);
+            connections.emplace(std::move(connection));
         }
         catch (std::exception const&)
         {
@@ -98,6 +112,12 @@ int main(int argc, char* argv[])
         if (threads.size() < std::thread::hardware_concurrency())
             threads.emplace_back(addConnection);
     }
+
+    for (auto& thread : threads)
+        thread.join();
+
+    for (auto const& connection : connections)
+        std::println("{0}", connection.string());
 
     return 0;
 }
