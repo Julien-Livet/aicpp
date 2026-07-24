409c1be07a83253596b87033448068f5cb27eaa6
Julien LIVET <julien.livet@univ-nantes.fr>
2026-07-24 09:15:59 +0200

    Refactor neuron creation in DslSystem to use lambda functions for Hodel integration

--- scripts/cpp_dsl_system.py
+++ scripts/cpp_dsl_system.py
@@ -89,9 +89,9 @@ for definition in primitiveDefinitions:
     content += f'    neurons.emplace("{trueName}"'
     content += ", Neuron{"
     content += f'"{trueName}"'
-    content += ", hodel::"
+    content += ", [] (std::vector<std::any> const& args) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(hodel::"
     content += name
-    content += ", std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});\n"
+    content += "); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});\n"
 
 content += """
     return neurons;
