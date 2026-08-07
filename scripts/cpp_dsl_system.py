7bed0f1c631eba70ace1dcf82e7846c10c8a2aba
Julien LIVET <julien.livet@univ-nantes.fr>
2026-08-07 18:37:35 +0200

    Refactor dslTypes: Update Container definitions and remove redundant function type from Any

--- scripts/cpp_dsl_system.py
+++ scripts/cpp_dsl_system.py
@@ -43,18 +43,17 @@ dslTypes["FrozenSet"] = frozenSetTypes
 
 containerTypes = dslTypes["FrozenSet"] + dslTypes["Grid"] + dslTypes["IntegerTuple"]
 
-dslTypes["Container"] = containerTypes + ["std::vector<Integer>"] + ["std::vector<Grid>"] + ["std::vector<Object>"] + ["std::vector<IntegerTuple>"] + ["std::vector<Cell>"] + ["std::vector<Indices>"]
+dslTypes["Container"] = containerTypes + ["std::vector<Integer>"] + ["std::vector<Grid>"] + ["std::vector<Object>"] + ["std::vector<IntegerTuple>"] + ["std::vector<Cell>"] + ["std::vector<Indices>"] + ["std::vector<std::function<std::any(std::any const&)> >"]
+dslTypes["ContainerContainer"] = dslTypes["Objects"] + dslTypes["IndicesSet"] + dslTypes["Grid"] + ["std::vector<Grid>"]
 dslTypes["Callable"] = ["std::function<std::any(std::vector<std::any> const&)>"]
 dslTypes["Container1"] = dslTypes["Container"]
 dslTypes["Container2"] = dslTypes["Container"]
-dslTypes["ContainerContainer"] = dslTypes["Objects"] + dslTypes["IndicesSet"] + dslTypes["Grid"] + ["std::vector<Grid>"]
+dslTypes["Container3"] = dslTypes["Container"]
 
 anyTypes = []
 
 for v in dslTypes.values():
     anyTypes += v
-
-anyTypes.remove("std::function<std::any(std::vector<std::any> const&)>")
 
 dslTypes["Any"] = anyTypes
 
@@ -182,6 +181,8 @@ content += """
     return neurons;
 }
 """
+
+content = content.replace("hodel::std::function", "std::function")
 
 with open("../src/aicpp/DslSystem.cpp", "w") as f:
     f.write(content)
