5b853ce03277f185a50e5e0c471a61427dadb8fc
Julien LIVET <julien.livet@univ-nantes.fr>
2026-08-09 17:37:49 +0200

    Update checkConnections function: Uncomment mapply connection for consistency

--- src/dsl_dataset.cpp
+++ src/dsl_dataset.cpp
@@ -314,7 +314,7 @@ void checkConnections(std::map<std::string, Neuron> const& variables, std::map<s
         {"first", "apply", "trim", "pair", "I", "hmirror", "I"},
         {"crop", "I", "astuple", "ZERO", "ZERO", "astuple", "halve", "halve", "last", "apply", "size", "objects", "vmirror", "I", "F", "F", "T", "halve", "halve", "first", "apply", "size", "objects", "vmirror", "I", "F", "F", "T"},
         {"first", "rapply", "insert", "trim", "initset", "hmirror", "I"},
-        //{"mapply"},
+        {"mapply", "trim", "pair", "I", "hmirror", "I"},
         //{"papply"},
         //{"mpapply"},
         //{"prapply"},
