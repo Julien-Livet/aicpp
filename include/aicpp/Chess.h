a12cace6318cdcae1899a30f1914bfb0ea8cb473
Julien LIVET <julien.livet@univ-nantes.fr>
2026-08-27 20:00:57 +0200

    Refactor chess types: replace Integer with int, update Position and Piece definitions

--- include/aicpp/Chess.h
+++ include/aicpp/Chess.h
@@ -9,37 +9,47 @@
 
 namespace chess
 {
-    typedef int8_t Integer;
-    typedef std::pair<Integer, Integer> Position;
+    typedef int Integer;
+
+    enum Kind : Integer
+    {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    };
+
+    enum Column : Integer
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H
+    };
+
+    enum Row : Integer
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight
+    };
+
+    typedef std::pair<Column, Row> Position;
     typedef bool Color;
-    typedef std::tuple<Color, Integer, Position> Piece;
+    typedef std::tuple<Color, Kind, Position> Piece;
     typedef std::set<Piece> Set;
     typedef std::pair<Set, Set> Board;
-
-    Integer constexpr A = 1;
-    Integer constexpr B = 2;
-    Integer constexpr C = 3;
-    Integer constexpr D = 4;
-    Integer constexpr E = 5;
-    Integer constexpr F = 6;
-    Integer constexpr G = 7;
-    Integer constexpr H = 8;
-
-    Integer constexpr One = 1;
-    Integer constexpr Two = 2;
-    Integer constexpr Three = 3;
-    Integer constexpr Four = 4;
-    Integer constexpr Five = 5;
-    Integer constexpr Six = 6;
-    Integer constexpr Seven = 7;
-    Integer constexpr Eight = 8;
-
-    Integer constexpr Pawn = 0;
-    Integer constexpr Knight = 1;
-    Integer constexpr Bishop = 2;
-    Integer constexpr Rook = 3;
-    Integer constexpr Queen = 4;
-    Integer constexpr King = 5;
 
     //Board initialBoard();
     std::any initialBoard(std::vector<std::any> const& args);
