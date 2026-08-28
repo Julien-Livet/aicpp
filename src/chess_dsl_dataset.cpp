6bdef1fc7a2bad90274a0d0c9d6fdd41c5715c7c
Julien LIVET <julien.livet@univ-nantes.fr>
2026-08-28 10:57:19 +0200

    Implement printBoard function and refactor buildConnection for piece movement logic

--- src/chess_dsl_dataset.cpp
+++ src/chess_dsl_dataset.cpp
@@ -26,71 +26,6 @@ struct PairLess
     }
 };
 
-Connection buildConnection(std::map<std::type_index, std::vector<std::reference_wrapper<Neuron const> > > const& variableNeuronsByOutputType,
-                           std::map<std::type_index, std::vector<std::reference_wrapper<Neuron const> > > const& neuronsByOutputType,
-                           size_t depth, std::type_index const& type)
-{
-    std::random_device rd;
-
-    if (!depth)
-    {
-        std::uniform_int_distribution<size_t> dist(0, variableNeuronsByOutputType.at(type).size() - 1);
-        Neuron const& neuron{variableNeuronsByOutputType.at(type).at(dist(rd))};
-
-        assert(neuron.inputTypes().empty());
-
-        return Connection{neuron, {}};
-    }
-
-    std::map<std::string, std::vector<std::reference_wrapper<Neuron const> > > namedNeurons;
-
-    for (auto const& neuron : neuronsByOutputType.at(type))
-        namedNeurons[neuron.get().name()].emplace_back(neuron);
-
-    std::vector<std::vector<std::reference_wrapper<Neuron const> > > nn;
-    nn.reserve(namedNeurons.size());
-
-    for (auto const& [n, v] : namedNeurons)
-        nn.emplace_back(v);
-
-    std::shuffle(nn.begin(), nn.end(), rd);
-    
-    while (nn.size())
-    {
-        auto neurons{nn.back()};
-        nn.pop_back();
-
-        std::shuffle(neurons.begin(), neurons.end(), rd);
-
-        while (neurons.size())
-        {
-            auto const neuron{neurons.back()};
-            neurons.pop_back();
-
-            try
-            {
-                std::vector<std::any> inputs;
-
-                for (auto const& inputType : neuron.get().inputTypes())
-                {
-                    Connection const& inputConnection{buildConnection(variableNeuronsByOutputType, neuronsByOutputType, depth - 1, inputType)};
-                    inputs.emplace_back(inputConnection);
-                }
-
-                return Connection{neuron.get(), inputs};
-            }
-            catch (std::exception const&)
-            {
-                break; //TODO: to remove?
-            }
-        }
-
-        break; //TODO: to remove?
-    }
-
-    throw std::runtime_error{"Wrong connection"};
-}
-
 void printBoard(chess::Board const& board)
 {
     std::vector<std::vector<char> > grid(8, std::vector<char>(8, '.'));
@@ -145,6 +80,156 @@ void printBoard(chess::Board const& board)
 
         std::cout << std::endl;
     }
+}
+
+Connection buildConnection(std::map<std::type_index, std::vector<std::reference_wrapper<Neuron const> > > const& variableNeuronsByOutputType,
+                           std::map<std::type_index, std::vector<std::reference_wrapper<Neuron const> > > const& neuronsByOutputType,
+                           size_t depth, std::type_index const& type)
+{
+    std::random_device rd;
+
+    if (!depth)
+    {
+        std::uniform_int_distribution<size_t> dist(0, variableNeuronsByOutputType.at(type).size() - 1);
+        Neuron const& neuron{variableNeuronsByOutputType.at(type).at(dist(rd))};
+
+        assert(neuron.inputTypes().empty());
+
+        return Connection{neuron, {}};
+    }
+
+    std::map<std::string, std::vector<std::reference_wrapper<Neuron const> > > namedNeurons;
+
+    for (auto const& neuron : neuronsByOutputType.at(type))
+        namedNeurons[neuron.get().name()].emplace_back(neuron);
+
+    std::vector<std::vector<std::reference_wrapper<Neuron const> > > nn;
+    nn.reserve(namedNeurons.size());
+
+    for (auto const& [n, v] : namedNeurons)
+        nn.emplace_back(v);
+
+    std::shuffle(nn.begin(), nn.end(), rd);
+    
+    while (nn.size())
+    {
+        auto neurons{nn.back()};
+        nn.pop_back();
+
+        std::shuffle(neurons.begin(), neurons.end(), rd);
+
+        while (neurons.size())
+        {
+            auto const neuron{neurons.back()};
+            neurons.pop_back();
+
+            auto const& inputTypes{neuron.get().inputTypes()};
+
+            try
+            {
+                if (neuron.get().name() == "movePiece")
+                {
+                    std::vector<std::any> inputs;
+
+                    Connection const& inputConnection{buildConnection(variableNeuronsByOutputType, neuronsByOutputType, depth - 1, inputTypes[0])};
+                    inputs.emplace_back(inputConnection);
+
+                    auto const board{std::any_cast<chess::Board>(inputConnection.output())};
+                    chess::Color const color{static_cast<bool>(depth % 2)};
+
+                    auto const positions{chess::movablePieces(board, color)};
+
+                    std::uniform_int_distribution<size_t> distribution1(0, positions.size() - 1);
+
+                    auto it1{positions.begin()};
+                    std::advance(it1, distribution1(rd));
+
+                    auto const from{*it1};
+
+                    auto const moves{std::any_cast<std::set<chess::Position> >(chess::pieceMoves({board, from}))};
+
+                    std::uniform_int_distribution<size_t> distribution2(0, moves.size() - 1);
+
+                    auto it2{moves.begin()};
+                    std::advance(it2, distribution2(rd));
+
+                    auto const to{*it2};
+
+                    auto posConnection =
+                    [&] (chess::Position const& position) -> Connection
+                    {
+                        Neuron const* columnNeuron{nullptr};
+
+                        for (auto const& neuron : variableNeuronsByOutputType.at(typeid(chess::Column)))
+                        {
+                            if (std::any_cast<chess::Column>(neuron.get().function()({})) == std::get<0>(position))
+                            {
+                                columnNeuron = &neuron.get();
+                                break;
+                            }
+                        }
+
+                        assert(columnNeuron);
+
+                        Neuron const* rowNeuron{nullptr};
+
+                        for (auto const& neuron : variableNeuronsByOutputType.at(typeid(chess::Row)))
+                        {
+                            if (std::any_cast<chess::Row>(neuron.get().function()({})) == std::get<1>(position))
+                            {
+                                rowNeuron = &neuron.get();
+                                break;
+                            }
+                        }
+
+                        assert(rowNeuron);
+
+                        Neuron const* positionNeuron{nullptr};
+
+                        for (auto const& neuron : neuronsByOutputType.at(typeid(chess::Position)))
+                        {
+                            if (neuron.get().name() == "position")
+                            {
+                                positionNeuron = &neuron.get();
+                                break;
+                            }
+                        }
+
+                        assert(positionNeuron);
+
+                        return Connection{*positionNeuron,
+                                          {Connection{*columnNeuron, {}},
+                                           Connection{*rowNeuron, {}}}};
+                    };
+
+                    inputs.emplace_back(posConnection(from));
+                    inputs.emplace_back(posConnection(to));
+
+                    return Connection{neuron.get(), inputs};
+                }
+                else
+                {
+                    std::vector<std::any> inputs;
+
+                    for (auto const& inputType : inputTypes)
+                    {
+                        Connection const& inputConnection{buildConnection(variableNeuronsByOutputType, neuronsByOutputType, depth - 1, inputType)};
+                        inputs.emplace_back(inputConnection);
+                    }
+
+                    return Connection{neuron.get(), inputs};
+                }
+            }
+            catch (std::exception const&)
+            {
+                break; //TODO: to remove?
+            }
+        }
+
+        break; //TODO: to remove?
+    }
+
+    throw std::runtime_error{"Wrong connection"};
 }
 
 int main(int argc, char* argv[])
