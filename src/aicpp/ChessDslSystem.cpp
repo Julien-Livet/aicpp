ae763276732cd55a28bf901e734f1e3876c87ee1
Julien LIVET <julien.livet@univ-nantes.fr>
2026-08-28 08:37:41 +0200

    Refactor chess DSL neurons to include pieces and positions, removing unused entries

--- src/aicpp/ChessDslSystem.cpp
+++ src/aicpp/ChessDslSystem.cpp
@@ -7,43 +7,36 @@ std::map<std::string, Neuron> aicpp::chessDslVariableNeurons()
 {
     std::map<std::string, Neuron> neurons;
 
-    neurons.emplace("Zero", Neuron{"Zero", [] (std::vector<std::any> const&) -> std::any { return chess::Zero; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("One", Neuron{"One", [] (std::vector<std::any> const&) -> std::any { return chess::One; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Two", Neuron{"Two", [] (std::vector<std::any> const&) -> std::any { return chess::Two; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Three", Neuron{"Three", [] (std::vector<std::any> const&) -> std::any { return chess::Three; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Four", Neuron{"Four", [] (std::vector<std::any> const&) -> std::any { return chess::Four; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Five", Neuron{"Five", [] (std::vector<std::any> const&) -> std::any { return chess::Five; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Six", Neuron{"Six", [] (std::vector<std::any> const&) -> std::any { return chess::Six; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Seven", Neuron{"Seven", [] (std::vector<std::any> const&) -> std::any { return chess::Seven; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Eight", Neuron{"Eight", [] (std::vector<std::any> const&) -> std::any { return chess::Eight; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Nine", Neuron{"Nine", [] (std::vector<std::any> const&) -> std::any { return chess::Nine; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Ten", Neuron{"Ten", [] (std::vector<std::any> const&) -> std::any { return chess::Ten; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Eleven", Neuron{"Eleven", [] (std::vector<std::any> const&) -> std::any { return chess::Eleven; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twelve", Neuron{"Twelve", [] (std::vector<std::any> const&) -> std::any { return chess::Twelve; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Thirteen", Neuron{"Thirteen", [] (std::vector<std::any> const&) -> std::any { return chess::Thirteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Fourteen", Neuron{"Fourteen", [] (std::vector<std::any> const&) -> std::any { return chess::Fourteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Fifteen", Neuron{"Fifteen", [] (std::vector<std::any> const&) -> std::any { return chess::Fifteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("sixteen", Neuron{"sixteen", [] (std::vector<std::any> const&) -> std::any { return chess::sixteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("seventeen", Neuron{"seventeen", [] (std::vector<std::any> const&) -> std::any { return chess::seventeen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("eighteen", Neuron{"eighteen", [] (std::vector<std::any> const&) -> std::any { return chess::eighteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("nineteen", Neuron{"nineteen", [] (std::vector<std::any> const&) -> std::any { return chess::nineteen; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty", Neuron{"Twenty", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_one", Neuron{"Twenty_one", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_one; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_two", Neuron{"Twenty_two", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_two; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_three", Neuron{"Twenty_three", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_three; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_four", Neuron{"Twenty_four", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_four; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_five", Neuron{"Twenty_five", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_five; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_six", Neuron{"Twenty_six", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_six; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("Twenty_seven", Neuron{"Twenty_seven", [] (std::vector<std::any> const&) -> std::any { return chess::Twenty_seven; }, std::vector<std::type_index>{}, typeid(chess::Integer)});
-    neurons.emplace("White", Neuron{"White", [] (std::vector<std::any> const&) -> std::any { return chess::White; }, std::vector<std::type_index>{}, typeid(chess::Color)});
-    neurons.emplace("Black", Neuron{"Black", [] (std::vector<std::any> const&) -> std::any { return chess::Black; }, std::vector<std::type_index>{}, typeid(chess::Color)});
+    neurons.emplace("Pawn", Neuron{"Pawn", [] (std::vector<std::any> const&) -> std::any { return chess::Pawn; }, std::vector<std::type_index>{}, typeid(chess::Pawn)});
+    neurons.emplace("Knight", Neuron{"Knight", [] (std::vector<std::any> const&) -> std::any { return chess::Knight; }, std::vector<std::type_index>{}, typeid(chess::Knight)});
+    neurons.emplace("Bishop", Neuron{"Bishop", [] (std::vector<std::any> const&) -> std::any { return chess::Bishop; }, std::vector<std::type_index>{}, typeid(chess::Bishop)});
+    neurons.emplace("Rook", Neuron{"Rook", [] (std::vector<std::any> const&) -> std::any { return chess::Rook; }, std::vector<std::type_index>{}, typeid(chess::Rook)});
+    neurons.emplace("Queen", Neuron{"Queen", [] (std::vector<std::any> const&) -> std::any { return chess::Queen; }, std::vector<std::type_index>{}, typeid(chess::Queen)});
+    neurons.emplace("King", Neuron{"King", [] (std::vector<std::any> const&) -> std::any { return chess::King; }, std::vector<std::type_index>{}, typeid(chess::King)});
+    neurons.emplace("A", Neuron{"A", [] (std::vector<std::any> const&) -> std::any { return chess::A; }, std::vector<std::type_index>{}, typeid(chess::A)});
+    neurons.emplace("B", Neuron{"B", [] (std::vector<std::any> const&) -> std::any { return chess::B; }, std::vector<std::type_index>{}, typeid(chess::B)});
+    neurons.emplace("C", Neuron{"C", [] (std::vector<std::any> const&) -> std::any { return chess::C; }, std::vector<std::type_index>{}, typeid(chess::C)});
+    neurons.emplace("D", Neuron{"D", [] (std::vector<std::any> const&) -> std::any { return chess::D; }, std::vector<std::type_index>{}, typeid(chess::D)});
+    neurons.emplace("E", Neuron{"E", [] (std::vector<std::any> const&) -> std::any { return chess::E; }, std::vector<std::type_index>{}, typeid(chess::E)});
+    neurons.emplace("F", Neuron{"F", [] (std::vector<std::any> const&) -> std::any { return chess::F; }, std::vector<std::type_index>{}, typeid(chess::F)});
+    neurons.emplace("G", Neuron{"G", [] (std::vector<std::any> const&) -> std::any { return chess::G; }, std::vector<std::type_index>{}, typeid(chess::G)});
+    neurons.emplace("H", Neuron{"H", [] (std::vector<std::any> const&) -> std::any { return chess::H; }, std::vector<std::type_index>{}, typeid(chess::H)});
+    neurons.emplace("One", Neuron{"One", [] (std::vector<std::any> const&) -> std::any { return chess::One; }, std::vector<std::type_index>{}, typeid(chess::One)});
+    neurons.emplace("Two", Neuron{"Two", [] (std::vector<std::any> const&) -> std::any { return chess::Two; }, std::vector<std::type_index>{}, typeid(chess::Two)});
+    neurons.emplace("Three", Neuron{"Three", [] (std::vector<std::any> const&) -> std::any { return chess::Three; }, std::vector<std::type_index>{}, typeid(chess::Three)});
+    neurons.emplace("Four", Neuron{"Four", [] (std::vector<std::any> const&) -> std::any { return chess::Four; }, std::vector<std::type_index>{}, typeid(chess::Four)});
+    neurons.emplace("Five", Neuron{"Five", [] (std::vector<std::any> const&) -> std::any { return chess::Five; }, std::vector<std::type_index>{}, typeid(chess::Five)});
+    neurons.emplace("Six", Neuron{"Six", [] (std::vector<std::any> const&) -> std::any { return chess::Six; }, std::vector<std::type_index>{}, typeid(chess::Six)});
+    neurons.emplace("Seven", Neuron{"Seven", [] (std::vector<std::any> const&) -> std::any { return chess::Seven; }, std::vector<std::type_index>{}, typeid(chess::Seven)});
+    neurons.emplace("Eight", Neuron{"Eight", [] (std::vector<std::any> const&) -> std::any { return chess::Eight; }, std::vector<std::type_index>{}, typeid(chess::Eight)});
+    neurons.emplace("White", Neuron{"White", [] (std::vector<std::any> const&) -> std::any { return chess::White; }, std::vector<std::type_index>{}, typeid(chess::White)});
+    neurons.emplace("Black", Neuron{"Black", [] (std::vector<std::any> const&) -> std::any { return chess::Black; }, std::vector<std::type_index>{}, typeid(chess::Black)});
 
     neurons.emplace("initialBoard", Neuron{"initialBoard", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::initialBoard); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
-    neurons.emplace("inPlayerSet", Neuron{"inPlayerSet", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::inPlayerSet); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
-    neurons.emplace("outPlayerSet", Neuron{"outPlayerSet", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::outPlayerSet); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
+    neurons.emplace("position", Neuron{"position", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::position); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
+    neurons.emplace("removedPieces", Neuron{"removedPieces", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::removedPieces); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
     neurons.emplace("pieceMoves", Neuron{"pieceMoves", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::pieceMoves); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
     neurons.emplace("movePiece", Neuron{"movePiece", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::movePiece); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
-    neurons.emplace("at", Neuron{"at", [] (std::vector<std::any> const&) -> std::any { return std::function<std::any(std::vector<std::any> const&)>(chess::at); }, std::vector<std::type_index>{}, typeid(std::function<std::any(std::vector<std::any> const&)>)});
 
     return neurons;
 }
@@ -53,22 +46,10 @@ std::map<std::string, Neuron> aicpp::chessDslPrimitiveNeurons()
     std::map<std::string, Neuron> neurons;
 
     neurons.emplace("initialBoard0", Neuron{"initialBoard", chess::initialBoard, std::vector<std::type_index>{}, typeid(chess::Board)});
-    neurons.emplace("inPlayerSet0", Neuron{"inPlayerSet", chess::inPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Color>)});
-    neurons.emplace("inPlayerSet1", Neuron{"inPlayerSet", chess::inPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Position>)});
-    neurons.emplace("inPlayerSet2", Neuron{"inPlayerSet", chess::inPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Types::Kind>)});
-    neurons.emplace("outPlayerSet0", Neuron{"outPlayerSet", chess::outPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Color>)});
-    neurons.emplace("outPlayerSet1", Neuron{"outPlayerSet", chess::outPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Position>)});
-    neurons.emplace("outPlayerSet2", Neuron{"outPlayerSet", chess::outPlayerSet, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Types::Kind>)});
-    neurons.emplace("pieceMoves0", Neuron{"pieceMoves", chess::pieceMoves, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Position>)});
-    neurons.emplace("pieceMoves1", Neuron{"pieceMoves", chess::pieceMoves, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Position)}, typeid(std::set<chess::Position>)});
-    neurons.emplace("pieceMoves2", Neuron{"pieceMoves", chess::pieceMoves, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Types::Kind)}, typeid(std::set<chess::Position>)});
-    neurons.emplace("movePiece0", Neuron{"movePiece", chess::movePiece, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color), typeid(chess::Position)}, typeid(chess::Board)});
-    neurons.emplace("movePiece1", Neuron{"movePiece", chess::movePiece, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Position), typeid(chess::Position)}, typeid(chess::Board)});
-    neurons.emplace("movePiece2", Neuron{"movePiece", chess::movePiece, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Types::Kind), typeid(chess::Position)}, typeid(chess::Board)});
-    neurons.emplace("at0", Neuron{"at", chess::at, std::vector<std::type_index>{typeid(std::set<chess::Color>), typeid(chess::Integer)}, typeid(chess::Color)});
-    neurons.emplace("at1", Neuron{"at", chess::at, std::vector<std::type_index>{typeid(std::set<chess::Position>), typeid(chess::Integer)}, typeid(chess::Position)});
-    neurons.emplace("at2", Neuron{"at", chess::at, std::vector<std::type_index>{typeid(std::set<chess::Types::Kind>), typeid(chess::Integer)}, typeid(chess::Types::Kind)});
-    neurons.emplace("at3", Neuron{"at", chess::at, std::vector<std::type_index>{typeid(std::set<chess::Position>), typeid(chess::Integer)}, typeid(chess::Position)});
+    neurons.emplace("position0", Neuron{"position", chess::position, std::vector<std::type_index>{typeid(chess::Column), typeid(chess::Row)}, typeid(chess::Position)});
+    neurons.emplace("removedPieces0", Neuron{"removedPieces", chess::removedPieces, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color)}, typeid(std::set<chess::Piece>)});
+    neurons.emplace("pieceMoves0", Neuron{"pieceMoves", chess::pieceMoves, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color), typeid(chess::Kind)}, typeid(std::set<chess::Position>)});
+    neurons.emplace("movePiece0", Neuron{"movePiece", chess::movePiece, std::vector<std::type_index>{typeid(chess::Board), typeid(chess::Color), typeid(chess::Kind), typeid(chess::Position)}, typeid(chess::Board)});
 
     return neurons;
 }
